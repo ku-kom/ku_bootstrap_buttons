61947106ca6a650b3d88cd7a72027c761ba6831c
nanna-dk <nanna@e-nanna.dk>
2022-12-12 12:28:25 +0100

    Init

---
+++ Configuration/Icons.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the package ku_phonebook.
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ * Sep 2022 Nanna Ellegaard, University of Copenhagen.
+ */
+
+ /**
+  * Icon registry
+  */
+
+defined('TYPO3_MODE') || die();
+
+return [
+    // icon identifier
+    'ku-bootstrap-button-icon' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:lwo_bootstrap_buttons/Resources/Public/Icons/Extension.svg',
+    ],
+];
