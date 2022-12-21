88e88ec3e2fe5e82a58cdb1011b50241987cfa26
nanna-dk <nanna@e-nanna.dk>
2022-12-21 13:29:53 +0100

    Backend preview

--- Classes/Backend/Preview/kuBootstrapButtonsPreviewRenderer.php
+++ Classes/Backend/Preview/kuBootstrapButtonsPreviewRenderer.php
@@ -14,21 +14,23 @@ use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Backend\View\BackendLayout\Grid\GridColumnItem;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Database\ConnectionPool;
-use TYPO3\CMS\Core\Database\Connection;
 
 class kuBootstrapButtonsPreviewRenderer implements PreviewRendererInterface
 {
+     /**
+     * @var string
+     */
+    protected $templatePath = 'EXT:ku_bootstrap_buttons/Resources/Private/Templates/PreviewRenderer/';
+
     protected ConnectionPool $connectionPool;
 
     public function __construct(
         
-    )
-    {
+    ) {
         $this->connectionPool = GeneralUtility::makeInstance(ConnectionPool::class);
     }
     public function renderPageModulePreviewHeader(GridColumnItem $item): string
     {
-        //$record = $item->getRecord();
         return '';
     }
 
@@ -36,12 +38,11 @@ class kuBootstrapButtonsPreviewRenderer implements PreviewRendererInterface
     {
         $record = $item->getRecord();
         $buttons = $this->getRecords($record['uid']);
-        
         $title = null;
 
         foreach ($buttons as $button) {
             $title .= $button['link_title'];
-        } 
+        }
 
         return $title;
     }
@@ -72,18 +73,22 @@ class kuBootstrapButtonsPreviewRenderer implements PreviewRendererInterface
         return BackendUtility::thumbCode($row, $table, $field, '', '', null, 0, '', '', false);
     }
 
+    /**
+     * Get records
+     *
+     * @return array
+     */
     protected function getRecords(int $uid): array
     {
-  
         return $this->connectionPool
             ->getConnectionForTable('tx_lwobootstrapbuttons_group_item')
             ->select(
-                ['*'], 
-                'tx_lwobootstrapbuttons_group_item', 
+                ['*'],
+                'tx_lwobootstrapbuttons_group_item',
                 [
                     'tt_content' => $uid
                 ]
             )
             ->fetchAllAssociative();
-    }   
+    }
 }
